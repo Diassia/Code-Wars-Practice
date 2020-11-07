bc8247c6448b07887c5d99281cc5258e0fcfc434
Kayleigh Shaw <shaw.kayleighemma@gmail.com>
2020-11-07 21:20:42 +0000

    Changes to code to include second function for number_of_repitions

--- repeatedcharacter.py
+++ repeatedcharacter.py
@@ -1,3 +1,5 @@
+# import collections
+
 # The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
 
 # Examples
@@ -6,26 +8,51 @@
 # "Success"  =>  ")())())"
 # "(( @"     =>  "))(("
 
-def letter_iterate(word):
-    for letter in word_string:
-        print(letter)
+# def letter_iterate(word):
+#     for letter in word_string:
+#         print(letter)
 
-#second function for "is x = y" with for loop
+# def iterate_string(letter, word): # counter attempt
+#     c = collections.Counter(word_string)
+    
+#     for letter in word_string:
+#         c[word] += 1      
 
-def duplicate_encode(word):
-    duplicate_string = ''
-    for index, letter in enumerate(word_string [:-1]): #enumerate will loop over something (e.g. the string and count)
-        if letter == word_string[index+1]:
-            duplicate_string += ')'
-        else:
-            duplicate_string += '('
-    print(duplicate_string)
+# #second function for "is x = y" with for loop
 
-    # iterate 'b' then iterate (to see if b matches), if b == b then print )
+# def duplicate_encode(word):
+#     duplicate_string = ''
+#     for index, letter in enumerate(word_string [:-1]): #enumerate will loop over something (e.g. the string and count)
+#         if letter == word_string[index+1]:
+#             duplicate_string += ')'
+#         else:
+#             iterate_string(letter, word)
+#             duplicate_string += '('
+#     print(duplicate_string)
 
-# count()
+#     # iterate 'b' then iterate (to see if b matches), if b == b then print )
 
-word_string = 'kayleigh!'
+# # count()
 
-letter_iterate(word_string)
-duplicate_encode(word_string)
+# word_string = 'davina'
+
+# letter_iterate(word_string)
+# duplicate_encode(word_string)
+
+
+
+def number_of_letter_repetitions():
+    count = 0
+    #for loop for iterating through letters and counting repetitions
+        #if repetitions is > 1
+            # count =+ 1
+        #else ???
+
+def iterate_string():
+    # new_string = ''
+    # for loop to iterate through letters
+        #if number_of_letter_repetitions > 1:
+            #print ) to new string
+        #else:
+            #print ( to new string
+
