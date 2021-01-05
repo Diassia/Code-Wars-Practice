917162b444f74dda3a6634daaaf4b4d47342f374
Kayleigh Shaw <shaw.kayleighemma@gmail.com>
2021-01-05 16:30:42 +0000

    Completed find_odd_int

--- meeting_names.py
+++ meeting_names.py
@@ -25,3 +25,8 @@ def meeting(s):
     return ''.join(names)
 
 print(meeting(s))
+
+
+# Top Codewars:
+# def meeting(s):
+#     return ''.join(sorted('({1}, {0})'.format(*(x.split(':'))) for x in s.upper().split(';')))
