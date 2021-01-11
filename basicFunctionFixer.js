b07aa957200080d07ae1a59ae459de1f480720ba
Kayleigh Shaw <shaw.kayleighemma@gmail.com>
2021-01-11 09:38:35 +0000

    basicFunctionFixer JS exercise complete

---
+++ basicFunctionFixer.js
@@ -0,0 +1,7 @@
+function addFive(num) {
+    return num + 5
+}
+
+console.log(addFive(5));
+console.log(addFive(0));
+console.log(addFive(-5));
--- out/vowelCount.js
+++ out/vowelCount.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.Kata = void 0;
 // We will consider a, e, i, o, u as vowels for this Kata (but not y).
 // The input string will only consist of lower case letters and/or spaces.
+// first attempt
 // export class Kata {
 //     static getCount(str: string) {
 //         let vowelList = 'aeiou';
@@ -16,15 +17,23 @@ exports.Kata = void 0;
 //         return vcount;
 //     }
 // }
+// regex attempt
 var Kata = /** @class */ (function () {
     function Kata() {
     }
     Kata.getCount = function (str) {
         var m = str.match(/[aeiou]/gi); // g makes it search the whole string, i makes it case-insensitive
-        return m === null ? 0 : m.length; // return 0 if there are no vowels
+        return m === null ? 0 : m.length; // return 0 if there are no vowels, otherwise will return number of vowels
     };
     return Kata;
 }());
 exports.Kata = Kata;
 console.log(Kata.getCount("abracadabra"));
+//Top CodeWars Solution:
+// export class Kata {
+//     static getCount(str: string) {
+//       let list = str.match(/[aeiou]/gi);
+//       return list ? list.length : 0;
+//     }
+//   }
 //# sourceMappingURL=vowelCount.js.map
