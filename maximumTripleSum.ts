f2ac9d4fcd944e8bee21fb1433c38defb0155ca0
Kayleigh Shaw <shaw.kayleighemma@gmail.com>
2021-01-14 10:49:54 +0000

    Exercise completed and works with test cases

--- maximumTripleSum.ts
+++ maximumTripleSum.ts
@@ -9,8 +9,11 @@
 // Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
 
 export function maxTriSum(nums: number[]): number {
-    let num_set = new Set(nums);
+    let num_set: Set<number> = new Set(nums.sort(function(a, b){return a - b}));
+    let num_array: number[] = Array.from(num_set);
+    let total_array: number[] = num_array.slice(-3);
+    return total_array.reduce((total, number) => total + number);
 }
 
-console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5]));
-console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4]));
+console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5])); // expect 32
+console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4])); // expect 18
--- out/maximumTripleSum.js
+++ out/maximumTripleSum.js
@@ -8,9 +8,15 @@ exports.maxTriSum = void 0;
 // Array/list numbers could be a mixture of positives , negatives and zeros .
 // Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
 function maxTriSum(nums) {
-    var num_set = new Set(nums);
+    var num_set = new Set(nums.sort(function (a, b) { return a - b; }));
+    var num_array = Array.from(num_set);
+    var total_array = num_array.slice(-3);
+    return total_array.reduce(function (total, number) { return total + number; });
+    // // return num_set[-1] + num_set[-2] + num_set[-3]
+    // return 0;
+    // (num_array[num_array.length-1]), (num_array[num_array.length-4])
 }
 exports.maxTriSum = maxTriSum;
-console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5]));
-console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4]));
+console.log(maxTriSum([2, 9, 13, 10, 5, 2, 9, 5])); // expect 17
+console.log(maxTriSum([2, 1, 8, 0, 6, 4, 8, 6, 2, 4])); // expect 18
 //# sourceMappingURL=maximumTripleSum.js.map
