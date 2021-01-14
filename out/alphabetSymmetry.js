0e23eb8e30cdb11fc76ddffe1f3fef60abd38366
Kayleigh Shaw <shaw.kayleighemma@gmail.com>
2021-01-14 15:41:08 +0000

    Completed and refactored menFromBoys TS

--- out/alphabetSymmetry.js
+++ out/alphabetSymmetry.js
@@ -6,9 +6,6 @@ exports.solve = void 0;
 // solve(["abode","ABc","xyzD"]) = [4, 3, 1]
 // See test cases for more examples.
 // Input will consist of alphabet characters, both uppercase and lowercase. No spaces.
-// let alphabet = {
-//     1: 'a', 2: 'b', 3: 'c', 4: 'd', 5: 'e', 6: 'f', 7: 'g', 8: 'h', 9: 'i', 10: 'j', 11: 'k', 12: 'l', 13: 'm', 14: 'n', 15: 'o', 16: 'p', 17: 'q', 18: 'r', 19: 's', 20: 't', 21: 'u', 22: 'v', 23: 'w', 24: 'x', 25: 'y', 26: 'z'
-// }
 function solve(arr) {
     var final_array = [];
     var letter_calculation = 0;
---
+++ sortMenFromBoys.ts
@@ -0,0 +1,51 @@
+// Scenario
+// Now that the competition gets tough it will Sort out the men from the boys .
+
+// Men are the Even numbers and Boys are the odd!alt!alt
+// Task
+// Given an array/list [] of n integers , Separate The even numbers from the odds , or Separate the men from the boys!alt!alt
+// Notes
+// Return an array/list where Even numbers come first then odds
+
+// Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+
+// Array/list size is at least *4*** .
+
+// Array/list numbers could be a mixture of positives , negatives .
+
+// Have no fear , It is guaranteed that no Zeroes will exists .!alt
+// Repetition of numbers in the array/list could occur , So (duplications are not included when separating).
+
+// Men: Even, ascending
+// Boys: Odd, decending
+
+
+
+export function menFromBoys(arr: number[]): number[] {
+    let menArray: number[] = [];
+    let boyArray: number[] = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 == 0) {
+            if (menArray.includes(arr[i])) {
+                continue;
+            } else {
+                menArray.push(arr[i]);
+            }
+        } else {
+            if (boyArray.includes(arr[i])) {
+                continue;
+            } else {
+                boyArray.push(arr[i]);
+            }
+        }
+    }
+
+    menArray.sort((a, b) => a - b);
+    boyArray.sort((a, b) => b - a);
+    
+    return menArray.concat(boyArray);
+}
+
+console.log(menFromBoys([7, 3, 14, 17])); // expect [14, 17, 7, 3]
+console.log(menFromBoys([2, 43, 95, 90, 37])); //expect [2, 90, 95, 43, 37]
